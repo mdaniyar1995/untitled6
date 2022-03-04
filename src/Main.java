51b06a22baa881f0235535fed5ab33b34718e0a5
Daniyar <mdaniyar1995@gmail.com>
2022-03-04 23:32:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+        int b=0;
+        try {
+            System.out.println(a/b);
+        } catch (Exception e) {
+            System.out.println("На ноль делить нельзя!");
+        }
+    }
+}
+
